ce4564b1e54c995543b779de7f6a0c09f9b29230
jhuhnke <jessica@flipsidecrypto.com>
2023-03-14 12:11:33 -0500

    front end wip

--- app.py
+++ app.py
@@ -18,72 +18,35 @@ prices = json_normalize(price_json)
 
 app_ui = ui.page_fluid(
     ui.tags.style((Path(__file__).parent / "style.css").read_text()),
+    ## Need to do: Social Links in the Header
+    ## Osmosis Zone Font
+    # Left most text not vertically aligned with text in output box
     ui.tags.h2({"class": "title"}, "Osmosis Impermanent Loss Calculator"),
     ui.row(
         ui.column(
-            5,
-            ui.tags.div(class_="box", children=[
-                ui.tags.div(class_="pool_selector", children=[
-                    ui.input_select("pool", "Select A Pool", pool_id['POOL_ID']),
-                ]),
-                ui.tags.div(class_="pool_tokens", children=[
-                    ui.row(
-                        ui.column(
-                            6, 
-                            ui.tags.h5({"class": "token_name"}, "Token 1"),
-                        ), 
-                        ui.column(
-                            6, 
-                            ui.tags.div(class_="name", children=[
-                                ui.output_text_verbatim("token1")
-                            ]),
-                        ),
-                    ),
-                    ui.row(
-                        ui.column(
-                            4, 
-                            ui.input_numeric("t1", "Number of Token 1", value = 1)
-                        ), 
-                    ),
-                    ui.row(
-                        ui.column(
-                            12, 
-                            ui.input_slider("rt1", "Token 1 Percentage Price Change", -100, 100, 0, step=0.5)
-                        ),
-                    ), 
-                    ui.row(
-                        ui.column(
-                            6, 
-                            ui.tags.h5({"class": "token_name"}, "Token 2"),
-                        ), 
-                        ui.column(
-                            6, 
-                            ui.tags.div(class_="name", children=[
-                                ui.output_text_verbatim("token2")
-                            ]),
-                        ),
-                    ),
-                    ui.row(
-                        ui.column(
-                            4, 
-                            ui.input_numeric("t2", "Number of Token 2", value = 1),
-                        )
-                    ),
-                    ui.row(
-                        ui.column(
-                            12, 
-                            ui.input_slider("rt2", "Token 2 Percentage Price Change", -100, 100, 0, step=0.5)
-                        ),
-                    ), 
-                ]), 
-            ]),
-        ),
-        ui.column(
-            6,
+            12,
             ui.tags.div(class_="box", children=[
                 ui.row(
                     ui.tags.h5({"class": "heading"}, "Input Values"),
                 ), 
+                ui.tags.div(class_="pool-info", children=[
+                    ui.row(
+                        ui.column(
+                            3
+                        ),
+                        ui.column(
+                            3, 
+                            ui.input_select("pool", "Select A Pool", pool_id['POOL_ID']),
+                        ), 
+                        ui.column(
+                            3,
+                            ui.input_numeric("fee", "Percent Pool Fee", value = 0.2, step=0.05) 
+                        ),
+                        ui.column(
+                            3,
+                        ),
+                    ),
+                ]),
                 ui.row(
                     ui.column(
                         3, 
@@ -101,6 +64,7 @@ app_ui = ui.page_fluid(
                         ui.tags.h6({"class": "output_text"}, "Total"),
                     ), 
                 ),
+                ### Need to do - Format Boxes & Get Token Images
                 ui.row(
                     ui.column(
                         3, 
@@ -124,11 +88,28 @@ app_ui = ui.page_fluid(
                     ), 
                     ui.column(
                         3, 
-                        ui.output_text_verbatim("num_t1"),
+                        ui.input_numeric("t1", None, value = 1),
                     ), 
                     ui.column(
                         3, 
-                        ui.output_text_verbatim("num_t2"),
+                        ui.input_numeric("t2", None, value = 1),
+                    ), 
+                    ui.column(
+                        3, 
+                    ), 
+                ),
+                ui.row(
+                    ui.column(
+                        3,
+                        ui.tags.h6({"class": "col-label"}, "% Price Change"),
+                    ), 
+                    ui.column(
+                        3, 
+                        ui.input_numeric("rt1", None, value = -2.0),
+                    ), 
+                    ui.column(
+                        3, 
+                        ui.input_numeric("rt2", None, value = 1),
                     ), 
                     ui.column(
                         3, 
@@ -320,16 +301,6 @@ def server(input: Inputs, output: Outputs, session: Session):
     
     @output
     @render.text
-    def num_t1(): 
-        return f"{str(input.t1())}"
-    
-    @output
-    @render.text
-    def num_t2(): 
-        return f"{str(input.t2())}"
-    
-    @output
-    @render.text
     async def mov_avg1(): 
         moving = await moving_avg()
         return f"${round(moving[0], 3)}"
@@ -375,21 +346,21 @@ def server(input: Inputs, output: Outputs, session: Session):
     async def f_lp1(): 
         start = await usd_values()
         end = await end_values()
-        return f"${round(start[0]+end[0], 3)}"
+        return f"${round((start[0]+end[0])*(100-input.fee())/100, 3)}"
     
     @output 
     @render.text
     async def f_lp2(): 
         start = await usd_values()
         end = await end_values()
-        return f"${round(start[1]+end[1], 3)}"
+        return f"${round(start[1]+end[1]*(100-input.fee())/100, 3)}"
     
     @output 
     @render.text
     async def f_lpt():
         start = await usd_values()
         end = await end_values()
-        return f"${round(start[2]+end[2], 3)}"
+        return f"${round(start[2]+end[2]*(100-input.fee())/100, 3)}"
     
     @output 
     @render.text
